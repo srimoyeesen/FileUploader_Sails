76f6861aa6bf7f1cdb1e2af3bcf978fe42d60236
Srimoyee <srimoyee.sen@gmail.com>
2018-08-13 12:36:05 +0530

    name handler changed

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -3,7 +3,6 @@ const path = require('path');
 const http = require('http');
 
 const uploadLocation = path.join(__dirname, '../../.tmp/uploads');
-console.log(uploadLocation);
 /**
  * FileController
  *
@@ -18,8 +17,12 @@ module.exports = {
       return res.json({ 'status': 'GET not allowed' });
 
     var uploadFile = req.file('uploadFile');
-    console.log(uploadFile);
-    uploadFile.upload(function onUploadComplete(err, files) {
+    var uploadOptions = {
+      saveAs: function (__newFileStream, cb) {
+        cb(null, __newFileStream.filename);
+      }
+    };
+    uploadFile.upload(uploadOptions,function onUploadComplete(err, files) {
       //  Files will be uploaded to .tmp/uploads
       if (err) return res.serverError(err);
       console.log(files);
@@ -28,6 +31,9 @@ module.exports = {
   },
 
   getFiles: function(req, res) {
+    if (req.method === 'POST')
+      return res.json({ 'status': 'POST not allowed' });
+
     fs.readdir(uploadLocation, function(err, files) {
       if (err) {
         res.json({
@@ -35,10 +41,7 @@ module.exports = {
         });
       }
       else {
-        // res.json({
-        //   'data': files
-        // });
-        res.render('pages/file_list.ejs', {data: files,clickHandler:"func1();"});
+        res.render('pages/file_list.ejs', {data: files});
       }
     });
   },
